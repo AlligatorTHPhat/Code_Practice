88162632821e1ff387d7a7aab757b6c434e1ed23
AlligatorTHPhat <164316763+AlligatorTHPhat@users.noreply.github.com>
2024-05-07 13:20:41 +0700

    Update C1.QT.01.cpp

--- UpCoder/Advanved Programming/Operator/C1.QT.01.cpp
+++ UpCoder/Advanved Programming/Operator/C1.QT.01.cpp
@@ -7,25 +7,31 @@ struct PhanSo {
     int tu;
     int mau;
 
+    int gcd(int a, int b)
+    {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
+
     void rutGon() {
-        int ucln = __gcd(this->tu, this->mau);
+        int ucln = gcd(this->tu, this->mau);
         this->tu /= ucln;
         this->mau /= ucln;
 
-        if (this->mau < 0) 
+        if (this->mau < 0)
             this->tu = -this->tu,
             this->mau = abs(this->mau);
 
         return;
     }
 
-    friend istream& operator >> (istream &in, PhanSo &p) {
+    friend istream& operator >> (istream& in, PhanSo& p) {
         in >> p.tu >> p.mau;
         p.rutGon();
         return in;
     }
 
-    friend ostream& operator << (ostream &out, PhanSo p) {
+    friend ostream& operator << (ostream& out, PhanSo p) {
         if (p.tu < 0) out << "-";
         out << abs(p.tu) << "/" << p.mau;
         return out;
@@ -36,24 +42,24 @@ struct SoPhuc {
     int thuc;
     int ao;
 
-    friend istream& operator >> (istream &in, SoPhuc &sp) {
+    friend istream& operator >> (istream& in, SoPhuc& sp) {
         in >> sp.thuc >> sp.ao;
         return in;
     }
 
-    friend ostream& operator << (ostream &out, SoPhuc sp) {
+    friend ostream& operator << (ostream& out, SoPhuc sp) {
         if (sp.thuc != 0) out << sp.thuc;
 
         if (sp.ao != 0) {
             if (sp.ao > 0 && sp.thuc != 0) out << "+";
             if (sp.ao < 0) out << "-";
-            if (abs(sp.ao) != 1) 
+            if (abs(sp.ao) != 1)
                 out << abs(sp.ao);
             out << "i";
-        } 
-        
+        }
+
         if (sp.thuc == 0 && sp.ao == 0) out << 0;
-        
+
         return out;
     }
 
@@ -80,7 +86,7 @@ struct SoPhuc {
 };
 
 int main() {
-    
+
     SoPhuc x; cin >> x;
     char c; cin >> c;
     cout << x << endl;
@@ -91,12 +97,14 @@ int main() {
         cout << i << endl;
         cout << x + i;
 
-    } else if (c == 'z') {
+    }
+    else if (c == 'z') {
         cin >> z;
         cout << z << endl;
         cout << x + z;
-        
-    } else if (c == 'p') {
+
+    }
+    else if (c == 'p') {
         cin >> p;
         cout << p << endl;
         cout << x + p;
