266062182374a80e433aadd8e15b6c933114fcec
AlligatorTHPhat <164316763+AlligatorTHPhat@users.noreply.github.com>
2024-05-08 13:33:09 +0700

    Update DEQUY7.cpp

--- UpCoder/Advanved Programming/Recursive/DEQUY7.cpp
+++ UpCoder/Advanved Programming/Recursive/DEQUY7.cpp
@@ -1,20 +1,16 @@
 #include <iostream>
-
 using namespace std;
 
-int Fibonacy(int n) {
-	if (n < 3) return 1;
-	return Fibonacy(n - 1) + Fibonacy(n - 2);
+int fibonacci(int n)
+{
+	if (n <= 1) return n;
+	return fibonacci(n - 1) + fibonacci(n - 2);
 }
-
-int main() {
-	int n; 
-	cin >> n;
-
-	for (int i = 1; i <= n; i++) {
-		cout << Fibonacy(i) << " ";
-	}
+int main()
+{
+    int n; cin >> n;
 	
+	for(int i = 1; i <= n; i++)
+        cout << fibonacci(i) << " ";
 	return 0;
-
 }
