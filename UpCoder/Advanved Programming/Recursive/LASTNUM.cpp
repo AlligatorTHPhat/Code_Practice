340e37e890f207afd904b63d83f1c5213a98e60b
AlligatorTHPhat <164316763+AlligatorTHPhat@users.noreply.github.com>
2024-05-13 21:34:10 +0700

    Update LASTNUM.cpp

--- UpCoder/Advanved Programming/Recursive/LASTNUM.cpp
+++ UpCoder/Advanved Programming/Recursive/LASTNUM.cpp
@@ -1,24 +1,23 @@
 #include <iostream>
-
 using namespace std;
 
 int sum_of_digit(int num) {
-	if (num < 10) return num;
-
-	return num % 10 + sum_of_digit(num / 10);
+    if (num < 10) return num;
+    return num % 10 + sum_of_digit(num / 10);
 }
 
 int Last_Number(int num) {
-	while(num >= 10) 
-		num = sum_of_digit(num);
-	return num;
+    while (num >= 10) {
+        num = sum_of_digit(num);
+    }
+    return num;
 }
 
 int main() {
-	int num;
-	cin >> num;
+    int num;
+    cin >> num;
 
-	cout << Last_Number(num);
+    cout << Last_Number(num);
 
-	return 0;
+    return 0;
 }
