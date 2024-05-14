817e7548545a580c60d4972dd08dacc0df13ec4e
AlligatorTHPhat <164316763+AlligatorTHPhat@users.noreply.github.com>
2024-05-14 15:40:53 +0700

    Create DEQUY9 (CACH 2).cpp

---
+++ UpCoder/Advanved Programming/Recursive/DEQUY9 (CACH 2).cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+long Sum(long n) {
+    if (n == 1) return 1;
+    return n + Sum(n - 1);
+}
+
+long S(long n) {
+    int res = 0;
+    for (int i = 1; i <= n; i++) {
+        res += Sum(i);
+    }
+    return res;
+}
+
+long Factorial(long n) {
+    if (n == 1) return 1;
+    return n * Factorial(n - 1);
+}
+
+long P(long n) {
+    int res = 0;
+    for (int i = 1; i <= n; i++) {
+        res += Factorial(i);
+    }
+    return res;
+}
+
+int main() {
+    long n;
+    cin >> n;
+    if (n <= 20) {
+        cout << "S(" << n << ") = " << S(n) << endl;
+        cout << "P(" << n << ") = " << P(n);
+    }
+
+    return 0;
+}
