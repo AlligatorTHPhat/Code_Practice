d3c4305ba4b064ad0fa04465e980ba5579d9151d
AlligatorTHPhat <164316763+AlligatorTHPhat@users.noreply.github.com>
2024-05-15 22:22:53 +0700

    Create CHUAN_HOA_TEN1.cpp

---
+++ Basic_Knowledge/String/CHUAN_HOA_TEN1.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+void Viet_Hoa(string& s) {
+	for (int i = 0; i < s.length(); i++) {
+		s[i] = toupper(s[i]);
+	}
+}
+
+void Chuan_Hoa(string& s) {
+	s[0] = toupper(s[0]);
+	for (int i = 1; i < s.length(); i++) {
+		s[i] = tolower(s[i]);
+	}
+}
+int main() {
+	int number;
+	cin >> number;
+	
+	cin.ignore();
+	map<string, int> mp;
+
+	while (number--) {
+		string s;
+		getline(cin, s);
+
+		vector<string> vec;
+		stringstream ss(s);
+
+		string tmp;
+		string res = "";
+		while (ss >> tmp)
+			vec.push_back(tmp);
+
+		for (int i = 0; i < vec.size() - 1; i++) {
+			Chuan_Hoa(vec[i]);
+			cout << vec[i];
+			if (i == vec.size() - 2) cout << ", ";
+			else cout << " ";
+		}
+		Viet_Hoa(vec[vec.size() - 1]);
+		cout << vec[vec.size() - 1] << endl;
+	}
+}
+
+//input
+// 1
+// Tran Hoang Phat
+
+//output
+// Tran Hoang, PHAT
