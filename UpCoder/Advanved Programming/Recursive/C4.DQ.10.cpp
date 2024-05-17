d160df0ac5a2bf127e101538ab0aac886f46c1be
AlligatorTHPhat <164316763+AlligatorTHPhat@users.noreply.github.com>
2024-05-17 15:09:36 +0700

    Create C4.DQ.10.cpp

---
+++ UpCoder/Advanved Programming/Recursive/C4.DQ.10.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+//1. Tạo hàm đệ quy
+int S(int n,int x) {
+    if (n == 0) return 1;
+    return x * S(n - 1, x);;
+}
+
+int main() {
+    int n, x;
+    cin >> n >> x;
+
+    cout << S(n,x);
+
+    return 0;
+}
