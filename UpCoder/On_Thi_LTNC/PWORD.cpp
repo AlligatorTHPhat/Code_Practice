5374290506ab5963dfc253aac259766c6f5008ef
AlligatorTHPhat <164316763+AlligatorTHPhat@users.noreply.github.com>
2024-05-18 15:15:42 +0700

    Create PWORD.cpp

---
+++ UpCoder/On_Thi_LTNC/PWORD.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	string str;
+	cin >> str;
+
+	int num = 0;
+	for (char c : str) {
+		if (isdigit(c)) num += c - '0';
+	}
+
+	cout << num;
+
+
+}
