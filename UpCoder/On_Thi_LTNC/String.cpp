11d58c32bdb0f56e7f088edeb39d9b9eb262136a
AlligatorTHPhat <164316763+AlligatorTHPhat@users.noreply.github.com>
2024-05-18 14:50:25 +0700

    Create String.cpp

---
+++ UpCoder/On_Thi_LTNC/String.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//Cpp kiểu dữ liệu string, là mảng chứa nhiều kí tự (char)
+//Chuyển từ int, double -> string thì sử dụng to_string();
+//Chèn thêm chuỗi insert(pos,str);
+//Xoá chuỗi từ vị trí pos đến độ dài len erase(pos,len);
+//Lấy ra chuỗi có vị trí độ dài len substr();
+//Xoá toàn bộ chuỗi clear();
+//Tìm chuỗi str từ vị trí pos find(str,pos); [trái qua phải]
+//Tìm chuỗi str từ vị trí pos rfind(str,pos); [phải qua trái]
+//Hằng số biểu thị ko hợp lệ string::npos
+//1 số hàm xl mảng, sort, reverse
+//Hàm xl ký tự
+//isdigit(str) : check string/char có phải là số
+//isalpha(str) : check string/char có phải là chữ cái
+//isalnum(str) : check string/char có phải là chữ cái hoặc số
+//isspace(str) : check string/char có phải là khoảng trắng
+//islower(str) : check string/char có phải là chữ in thường
+//isupper(str) : check string/char có phải là chữ in hoa
+//tolower(str) : chuyển sang chữ thường
+//toupper(str) : chuyển sang chữ hoa
+//stoi(str) : string -> int
+//stod(str) : string -> double
+//stoll(str) : tring -> ;long long
+int main() {
+
+  return 0;
+}
