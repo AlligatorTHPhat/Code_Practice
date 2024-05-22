66fc0acfed8fd9e372004c76fe5434e195b11640
AlligatorTHPhat <164316763+AlligatorTHPhat@users.noreply.github.com>
2024-05-22 15:51:31 +0700

    Update TKTUANTU.cpp

--- UpCoder/Data Structure/Find/TKTUANTU.cpp
+++ UpCoder/Data Structure/Find/TKTUANTU.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+#include <vector>
 
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio;
+	cin.tie(0); cout.tie(0);
+
+	int n, x; 
+	cin >> n >> x;
+
+	vector<int> vec(n);
+
+	for (int i = 0; i < vec.size(); i++)
+		cin >> vec[i];
+
+    int res = -1;
+	for (int i = 0; i < vec.size(); i++) {
+		if (vec[i] == x) { res = i; break; }
+	}
+	
+	cout << res;
+
+	return 0;
+}
