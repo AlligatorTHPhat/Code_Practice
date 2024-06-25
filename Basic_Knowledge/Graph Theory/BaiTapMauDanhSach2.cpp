abb7ba71a4bcb74e0b0db83422ba1ee01484b387
AlligatorTHPhat <164316763+AlligatorTHPhat@users.noreply.github.com>
2024-06-25 16:06:51 +0700

    Create BaiTapMauDanhSach2.cpp

---
+++ Basic_Knowledge/Graph Theory/BaiTapMauDanhSach2.cpp
@@ -0,0 +1,47 @@
+//Chuyen doi Danh Sach Canh (vo huong) thanh Danh Sach Ke
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int n, m;
+vector <int> adj[1001]; //adj[i] : luu danh sach ke cua dinh i
+ 
+int main() {
+	cin >> n >> m;
+
+	for (int i = 0; i < m; i++) {
+		int x, y; cin >> x >> y;
+		adj[x].push_back(y);
+		adj[y].push_back(x);
+	}
+
+	for (int i = 1; i <= n; i++) {
+		cout << i << " : ";
+		for (int x : adj[i]) {
+			cout << x << " ";
+		}
+		cout << endl;
+	}
+}
+/*
+Input :
+	5 9
+	1 2
+	1 3
+	1 4
+	2 3
+	2 4
+	2 5
+	3 5
+	3 5
+	4 5
+
+Output :
+	1 : 2 3 4
+	2 : 1 3 4 5
+	3 : 1 2 5 5
+	4 : 1 2 5
+	5 : 2 3 3 4
+*/ 
