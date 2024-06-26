692bd2931710988f19a17cc5bed8fbbb1dca225d
AlligatorTHPhat <164316763+AlligatorTHPhat@users.noreply.github.com>
2024-06-26 14:39:00 +0700

    Create DanhSachKe -->DanhSachCanh.cpp

---
+++ Basic_Knowledge/Graph Theory/ Theory /DanhSachKe -->DanhSachCanh.cpp
@@ -0,0 +1,55 @@
+//Danh Sach Ke --> Danh Sach Canh
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+int n;
+
+vector<int> adj[1001];
+vector<pair<int, int>> edge;
+
+int values[1001][1001];
+
+int main() {
+	//Nhap kich thuoc Ma Tran
+	cin >> n;
+	cin.ignore();
+	//Nhap Danh Sach Ke
+	for (int i = 1; i <= n; i++) {
+		string s, num;
+		getline(cin, s);
+		stringstream ss(s);
+		while (ss >> num) {
+			if (stoi(num) > i) {
+				edge.push_back({ i,stoi(num) });
+			}
+		}
+	}
+	//Xuat Danh Sach Canh
+	for (auto it : edge) {
+		cout << it.first << " " << it.second << endl;
+	}
+}
+
+/*
+Input :
+	5
+	2 3 4
+	1 3 4 5
+	1 2 4 5
+	1 2 3 5
+	2 3 4
+Output :
+	1 2
+	1 3
+	1 4
+	2 3
+	2 4
+	2 5
+	3 4
+	3 5
+	4 5
+*/
