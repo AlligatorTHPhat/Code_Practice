8ae3db7b95657cf24ad2b44722e41218bf0d7d34
AlligatorTHPhat <164316763+AlligatorTHPhat@users.noreply.github.com>
2024-07-03 21:14:14 +0700

    Update QTaiPSo.cpp

--- UpCoder/Advanved Programming/Operator/QTaiPSo.cpp
+++ UpCoder/Advanved Programming/Operator/QTaiPSo.cpp
@@ -1,58 +1,50 @@
 #include <iostream>
+
 using namespace std;
 
-struct Fraction
-{
+struct Fraction {
 	int numer, denom;
 
-	int gcd(int a, int b)
-	{
-		if (b == 0) return a;
-		return gcd(b, a % b);
-	}
-
-	void Simplifies()
-	{
-    	int d = gcd(this->numer, this->denom);
-    	this->numer /= d;
-    	this->denom /= d;
-    	return;
-    }
-
-	friend istream& operator >> (istream& is, Fraction& f)
-	{
+	friend istream& operator >> (istream& is, Fraction& f) {
 		is >> f.numer >> f.denom;
 		return is;
 	}
 
-	friend ostream& operator << (ostream& os, Fraction f)
-	{
+	friend ostream& operator << (ostream& os, Fraction f) {
 		f.Simplifies();
 		os << f.numer << "/" << f.denom;
 		return os;
 	}
 
-	Fraction operator + (Fraction f)
-	{
-		Fraction res; 
+	int gcd(int a, int b) {
+		if (b == 0) return a >= 0 ? a : -a;
+		return gcd(b, a % b);
+	}
+
+	void Simplifies() {
+		int d = gcd(this->numer, this->denom);
+		this->numer /= d;
+		this->denom /= d;
+		return;
+	}
+
+	Fraction operator + (Fraction f) {
+		Fraction res;
 		res.numer = this->numer * f.denom + this->denom * f.numer;
 		res.denom = this->denom * f.denom;
 		return res;
 	}
 
-	bool operator  == (Fraction f)
-	{
+	bool operator == (Fraction f) {
 		return this->numer * f.denom == this->denom * f.numer;
 	}
 
-	bool operator != (Fraction f)
-	{
-		return ((*this) != f);
+	bool operator != (Fraction f) {
+		return (*(this) != f);
 	}
-};
 
-int main()
-{
+};
+int main() {
 	Fraction a, b;
 	cin >> a >> b;
 	cout << a + b;
