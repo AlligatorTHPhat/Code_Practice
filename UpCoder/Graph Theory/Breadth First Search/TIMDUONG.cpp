0cea439bbcfa75bcd61cbe754c286251c6a0fb4a
AlligatorTHPhat <164316763+AlligatorTHPhat@users.noreply.github.com>
2024-07-04 19:48:42 +0700

    Update TIMDUONG.cpp

--- UpCoder/Graph Theory/Breadth First Search/TIMDUONG.cpp
+++ UpCoder/Graph Theory/Breadth First Search/TIMDUONG.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <vector>
 #include <cstring>
+#include <vector>
 #include <queue>
 
 using namespace std;
@@ -10,25 +10,24 @@ int cnt[1001];
 vector<int> adj[1001];
 bool visited[1001];
 
-
 void BFS(int S) {
 	queue<int> que;
 	que.push(S);
 	visited[S] = true;
 	cnt[S] = 0;
-	
+
 	while (!que.empty()) {
 		int q = que.front();
 		que.pop();
-		for (auto x : adj[q]) {
+		for (int x : adj[q]) {
 			if (!visited[x]) {
 				que.push(x);
 				visited[x] = true;
-				cnt[x] = cnt[q] + 1;			}		
+				cnt[x] = cnt[q] + 1;
+			}	
 		}
 	}
 }
-
 int main() {
 	cin >> Nv >> Ne >> S >> T;
 
@@ -37,12 +36,10 @@ int main() {
 		adj[x].push_back(y);
 		adj[y].push_back(x);
 	}
-	memset(visited, false,sizeof(visited));
+	memset(visited, false, sizeof(visited));
 	memset(cnt, -1, sizeof(cnt));
 
 	BFS(S);
 
 	cout << cnt[T];
-	
-	return 0;
 }
