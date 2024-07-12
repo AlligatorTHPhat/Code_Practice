d1771109130c9619f21ae84cd04c591864186ce3
AlligatorTHPhat <164316763+AlligatorTHPhat@users.noreply.github.com>
2024-07-12 19:03:00 +0700

    Update INAN.cpp

--- UpCoder/Basic Programming/If-Else/INAN.cpp
+++ UpCoder/Basic Programming/If-Else/INAN.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+#include<cmath>
 
+using namespace std;
+
+int main() {
+
+	int T;
+	long long P, C, res;
+
+	cin >> T >> P >> C;
+
+	1 <= T; T <= 2;
+	1 <= P; P <= pow(10, 9);
+	1 <= C; C <= pow(10, 9);
+
+	if (T == 1) {
+		res = P * C;
+	}
+
+	else if (T == 2) {
+		res = (P / 2 + P % 2) * C;
+	}
+
+	cout << res;
+	return 0;
+}
