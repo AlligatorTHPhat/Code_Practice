702d96b7160abe246b66a2ed87d891588e245be1
AlligatorTHPhat <164316763+AlligatorTHPhat@users.noreply.github.com>
2024-07-23 15:18:40 +0700

    Update SAPXEP01.cpp

--- UpCoder/Data Structure/Sort/SAPXEP01.cpp
+++ UpCoder/Data Structure/Sort/SAPXEP01.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-int values[100], array_size = 0;
+int array_size, values[100];
 
 /*
 void swap(dataptype& a, datatype& b) {
@@ -13,27 +13,28 @@ void swap(dataptype& a, datatype& b) {
 }
 */
 
-void input(int values[], int& array_size) {
+void input(int& array_size, int values[]) {
 	cin >> array_size;
 	for (int i = 0; i < array_size; i++) {
 		cin >> values[i];
 	} return;
 }
 
-void Insertion_Sort(int values[], int array_size) {
+void Insertion_Sort(int array_size, int values[]) {
+	int j;
 	for (int i = 1; i < array_size / 2; i++) {
-		int j = i;
+		j = i;
 		while ((j > 0) && (values[j - 1] > values[j])) {
 			swap(values[j - 1], values[j]);
 			j--;
-		}
+		} 
 	} return;
 }
 
-void Reverse_Insertion(int values[], int array_size) {
-	int start = ((array_size + 1) / 2);
+void Reverse_Sort(int array_size, int values[]) {
+	int start = (array_size + 1) / 2, j;
 	for (int i = start + 1; i < array_size; i++) {
-		int j = i;
+		j = i;
 		while ((j > start) && (values[j - 1] < values[j])) {
 			swap(values[j - 1], values[j]);
 			j--;
@@ -41,13 +42,17 @@ void Reverse_Insertion(int values[], int array_size) {
 	} return;
 }
 
-int main() {
-	input(values, array_size);
-	Insertion_Sort(values, array_size);
-	Reverse_Insertion(values, array_size);
-
-	for (int i = 0; i < array_size; i++) {
+void output(int array_size, int values[]) {
+	for (int i = 0; i < array_size; i++)
 		cout << values[i] << " ";
-	} return 0;
+	return;
 }
 
+int main() {
+	input(array_size, values);
+	Insertion_Sort(array_size, values);
+	Reverse_Sort(array_size, values);
+	output(array_size, values);
+	
+	return 0;
+}
