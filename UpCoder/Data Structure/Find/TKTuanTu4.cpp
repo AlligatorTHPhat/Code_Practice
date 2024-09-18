62ac8e99dc1f3da51f6ae38c4cb1325ccd7b9225
AlligatorTHPhat <164316763+AlligatorTHPhat@users.noreply.github.com>
2024-09-18 09:32:44 +0700

    Update TKTuanTu4.cpp

--- UpCoder/Data Structure/Find/TKTuanTu4.cpp
+++ UpCoder/Data Structure/Find/TKTuanTu4.cpp
@@ -1,38 +1,59 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-void input(int& x, int values[], int& size) {
+vector<int> r;
+
+void input(int& x, vector<int>& vec)
+{
 	cin >> x;
 
-	int tmp;
-	while (cin >> tmp) {
-		values[size++] = tmp;
-	} return;
-}
+	vec.resize(0);
 
-void linear_search(int x, int values[], int size) {
-	int res = -1, max = -1e6;
-	for (int i = 0; i < size; i++) {
-		if (values[i] <= x && values[i] >= max) {
-			res = i; max = values[i];
-		}
+	int n;
+	while (cin >> n)
+	{
+		vec.push_back(n);
 	}
-
-	if (res != -1) {
-		for (int i = 0; i < size; i++) {
-			if (values[i] == values[res]) cout << i << " ";
-		} return;
-	} cout << res;
 }
 
-int main() {
+int sequential_search(int x, vector<int>vec)
+{
+	int res = -1, max = -10e6;
+	for (int i = 0; i < vec.size(); i++)
+		if (vec[i] <= x && max < vec[i]) { res = i, max = vec[i]; }
+
+	return res;
+}
+
+void print_result(int x, vector<int> vec)
+{
+	int val = sequential_search(x, vec);
+	if (val != -1)
+	{
+		for (int i = 0; i < vec.size(); i++)
+			if(vec[i] == vec[val]) r.push_back(i);
+		for (auto v : r) cout << v << " ";
+	}
+	else cout << -1;
+}
+
+int main()
+{
+	ios::sync_with_stdio;
+	cin.tie(0); cout.tie(0);
+
 	int x;
-	int values[100], size = 0;
+	vector<int> vec;
 
-	input(x, values, size);
+	input(x, vec);
 
-	linear_search(x, values, size);
+	print_result(x, vec);
 
-
+	return 0;
 }
+
+
+//4
+//2 1 3 5 3 1
