43bbeaa7947047e816fb5ed9ea1fa382a804032c
AlligatorTHPhat <164316763+AlligatorTHPhat@users.noreply.github.com>
2024-09-18 08:32:40 +0700

    Update TKTUANTU.cpp

--- UpCoder/Data Structure/Find/TKTUANTU.cpp
+++ UpCoder/Data Structure/Find/TKTUANTU.cpp
@@ -36,7 +36,6 @@ int main()
 	return 0;
 }
 
-
 //#include <iostream>
 //
 //using namespace std;
@@ -70,36 +69,3 @@ int main()
 //	return 0;
 //}
 
-//Other way
-
-// #include <iostream>
-
-// using namespace std;
-
-// void input(int &n, int &x, int values[]) {
-// 	cin >> n >> x;
-// 	for (int i = 0; i < n; i++)
-// 		cin >> values[i];
-// }
-
-// void Sequential_Search(int n, int x, int values[]) {
-// 	int res = -1;
-// 	for (int i = 0; i < n; i++) {
-// 		{
-// 			if (x == values[i]) {
-// 				res = i;
-// 				break;
-// 			}
-// 		}
-// 	} cout << res;
-// }
-
-// int main() {
-// 	int n, x;
-// 	int values[100];
-
-// 	input(n, x, values);
-
-// 	Sequential_Search(n, x, values);
-
-// }
