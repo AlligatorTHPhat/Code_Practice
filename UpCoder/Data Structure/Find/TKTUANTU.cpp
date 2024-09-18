fe93a1479805a2d40fb33d135a3de4b04dca50b9
AlligatorTHPhat <164316763+AlligatorTHPhat@users.noreply.github.com>
2024-09-18 07:43:00 +0700

    Update TKTUANTU.cpp

--- UpCoder/Data Structure/Find/TKTUANTU.cpp
+++ UpCoder/Data Structure/Find/TKTUANTU.cpp
@@ -3,27 +3,72 @@
 
 using namespace std;
 
-int main() {
-	ios::sync_with_stdio;
-	cin.tie(0); cout.tie(0);
-
-	int n, x; 
+void input(int& n, int& x, vector<int>& vec)
+{
 	cin >> n >> x;
-
-	vector<int> vec(n);
 
 	for (int i = 0; i < vec.size(); i++)
 		cin >> vec[i];
 
-    int res = -1;
-	for (int i = 0; i < vec.size(); i++) {
-		if (vec[i] == x) { res = i; break; }
+}
+
+int sequential_search(int n, int x, vector<int>vec)
+{
+	int result = -1;
+	for (int i = 0; i < vec.size(); i++)
+	{
+		if (vec[i] == x) { result = i; break; }
 	}
-	
-	cout << res;
+
+	return result;
+}
+
+int main()
+{
+	ios::sync_with_stdio;
+	cin.tie(0); cout.tie(0);
+
+	int n, x;
+	vector<int> vec(n);
+	input(n, x, vec);
+	cout << sequential_search(n, x, vec);
 
 	return 0;
 }
+
+
+//#include <iostream>
+//
+//using namespace std;
+//
+//void input(int& n,int& x, int values[])
+//{
+//	cin >> n >> x;
+//
+//	for (int i = 0; i < n; i++)
+//		cin >> values[i];
+//
+//	return;
+//}
+//
+//int sequential_search(int n, int x, int values[])
+//{
+//	int result = -1;
+//	for (int i = 0; i < n; i++)
+//	{
+//		if (values[i] == x)
+//			result = i;
+//	}
+//	return result;
+//}
+//
+//int main() {
+//	int n, x, values[100];
+//	input(n, x, values);
+//	cout << sequential_search(n, x, values);
+//
+//	return 0;
+//}
 
 //Other way
 
