bfa443388737fc88b29f88082c53e48ac8ae5c1c
AlligatorTHPhat <164316763+AlligatorTHPhat@users.noreply.github.com>
2024-09-18 07:50:50 +0700

    Update TKNhiPhan.cpp

--- UpCoder/Data Structure/Find/TKNhiPhan.cpp
+++ UpCoder/Data Structure/Find/TKNhiPhan.cpp
@@ -1,24 +1,33 @@
 #include <iostream>
 using namespace std;
 
-int binary_search(int size,int values[], int target) {
-	int left = 0, right = target - 1;
-	while (left <= right) {
+void input(int& size, int& target, int values[])
+{
+	cin >> size >> target;
+	for (int i = 0; i < size; i++)
+	{
+		cin >> values[i];
+	}
+}
+
+int binary_search(int size, int target, int values[])
+{
+	int left = 0, right = size - 1;
+	while (left <= right)
+	{
 		int mid = (left + right) / 2;
 		if (values[mid] == target) return mid;
 		else if (values[mid] < target) left = mid + 1;
-		else if (values[mid]) right = mid - 1;
+		else if (values[mid] > target) right = mid - 1;
 	}
 	return -1;
 }
 
-int main() {
-	int size, values[100], target;
-	cin >> size >> target;
-	for (int i = 0; i < size; i++) 
-		cin >> values[i];
+int main()
+{
+	int size, target, values[100];
+	input(size, target, values);
+	cout << binary_search(size, target, values);
 
-	cout << binary_search(size, values, target);
-	
 	return 0;
 }
