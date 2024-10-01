146f2344990727ecbf3a58619753814db3d9ce82
AlligatorTHPhat <164316763+AlligatorTHPhat@users.noreply.github.com>
2024-10-02 06:53:14 +0700

    Update ExampleLinkedList.cpp

--- UpCoder/Data Structure/LinkedList/ExampleLinkedList.cpp
+++ UpCoder/Data Structure/LinkedList/ExampleLinkedList.cpp
@@ -1,84 +1,80 @@
 #include <iostream>
+
 using namespace std;
 
 template <class DataType>
 class Node {
 private:
     DataType data;
-    Node<DataType>* next;  // Khai báo con trỏ next với template
+    Node<DataType>* next; 
 
 public:
-    // Constructor
-    Node(DataType val) {
-        data = val;
-        next = nullptr;
+    Node(DataType val) : data(val), next(nullptr) {}
+
+    DataType get_Data() {
+        return data;
     }
 
     void display() {
         cout << data << " ";
     }
 
-    // Getter và Setter cho next
-    Node<DataType>* getNext() {
+    Node<DataType>* get_Next() {
         return next;
     }
 
-    void setNext(Node<DataType>* nextNode) {
-        next = nextNode;
+    void set_Next(Node<DataType>* next_Node) {
+        next = next_Node;
     }
 };
 
 template <class DataType>
 class LinkedList {
 private:
-    Node<DataType>* head;  // Lưu địa chỉ đầu tiên
-    Node<DataType>* tail;  // Lưu địa chỉ cuối cùng
-    int size;              // Kích thước của danh sách
+    Node<DataType>* head;  
+    Node<DataType>* tail;  
+    int size_LK;              
 
 public:
-    LinkedList();  // Constructor
-    ~LinkedList(); // Destructor
-    void add_Head(DataType data);  // Thêm phần tử vào đầu danh sách
-    void display();  // Hiển thị danh sách
+    LinkedList();  
+    ~LinkedList(); 
+    void add_Head(DataType data);  
+    void display();  
 };
 
 template<class DataType>
-LinkedList<DataType>::LinkedList() {
-    head = nullptr;
-    tail = nullptr;
-    size = 0;
-}
+LinkedList<DataType>::LinkedList() : head(nullptr), tail(nullptr), size_LK(0) {}
 
 template<class DataType>
 LinkedList<DataType>::~LinkedList() {
-    // Hủy tất cả các Node khi danh sách bị hủy
     Node<DataType>* temp;
     while (head != nullptr) {
         temp = head;
-        head = head->getNext();
+        head = head->get_Next();
         delete temp;
     }
 }
 
 template<class DataType>
 void LinkedList<DataType>::add_Head(DataType data) {
-    Node<DataType>* new_Node = new Node<DataType>(data);  // Tạo Node mới
+    Node<DataType>* new_Node = new Node<DataType>(data);  
 
     if (head == nullptr) {
-        head = tail = new_Node;  // Nếu danh sách rỗng, Node mới là đầu và đuôi
-    } else {
-        new_Node->setNext(head);  // Node mới trỏ đến Node đầu hiện tại
-        head = new_Node;  // Cập nhật head thành Node mới
+        head = tail = new_Node;
     }
-    ++size;
+    else {
+        new_Node->set_Next(head);
+        head = new_Node;
+    }
+    ++size_LK;
 }
 
 template<class DataType>
 void LinkedList<DataType>::display() {
     Node<DataType>* walker = head;
     while (walker != nullptr) {
-        walker->display();  // Gọi hàm display của Node
-        walker = walker->getNext();  // Duyệt qua Node tiếp theo
+        walker->display();  
+        walker = walker->get_Next();  
     }
     cout << endl;
 }
@@ -87,13 +83,13 @@ int main() {
     LinkedList<int> l;
     int x;
     
-    // Nhập dữ liệu từ người dùng và thêm vào danh sách liên kết
-    cout << "Nhập các số nguyên (nhập -1 để kết thúc): ";
-    while (cin >> x && x != -1) {
+    cout << "Nhap cac gia tri: ";
+    while (cin >> x) {
         l.add_Head(x);
     }
 
-    cout << "Danh sách liên kết: ";
-    l.display();  // Hiển thị danh sách
+    cout << "Danh sach lien ket: ";
+    l.display();  
+
     return 0;
 }
