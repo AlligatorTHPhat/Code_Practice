3ea900c9ddaf884aff1ed78b44b9c71c7ff8bdd9
AlligatorTHPhat <164316763+AlligatorTHPhat@users.noreply.github.com>
2024-10-31 22:48:52 +0700

    Update TKTuanTu3.cpp

--- UpCoder/Data Structure/Find/TKTuanTu3.cpp
+++ UpCoder/Data Structure/Find/TKTuanTu3.cpp
@@ -1,43 +1,97 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
-void input(int& x, vector<int>& vec)
+template <typename DataType>
+void input(int& _size, DataType& find, DataType values[])
 {
-	cin >> x;
-
-	vec.resize(0);
-
-	int n;
-	while (cin >> n)
+	/*cin >> _size >> find;
+	for (int i = 0; i < _size; i++)
 	{
-		vec.push_back(n);
+		cin >> values[i];
+	}*/
+
+	cin >> find;
+
+	DataType temp;
+	while (cin >> temp)
+	{
+		values[_size++] = temp;
 	}
 }
 
-int sequential_search(int x, vector<int>vec)
+template <typename DataType>
+int sequential_search(int _size, DataType find, DataType values[])
 {
-	int res = -1, max = -10e6;
-	for (int i = 0; i < vec.size(); i++)
-		if (vec[i] <= x && max < vec[i]) { res = i, max = vec[i]; };
+	int max = -1e6, pos = -1;
+	for (int i = 0; i < _size; i++)
+	{
+		if (max < values[i] && values[i] <= find)
+		{
+			max = values[i], pos = i;
+		}
+	}
+	return pos;
+}
 
-	return res;
+template <typename DataType>
+void print_sequential_search(int _size, DataType find, DataType values[])
+{
+	cout << sequential_search(_size, find, values);
 }
 
 int main()
 {
-	ios::sync_with_stdio;
-	cin.tie(0); cout.tie(0);
+	int n = 0;
+	int x, values[100];
 
-	int x;
-	vector<int> vec;
+	input<int>(n, x, values);
 
-	input(x, vec);
-	cout << sequential_search(x, vec);
+	print_sequential_search<int>(n, x, values);
 
 	return 0;
 }
+
+// #include <iostream>
+// #include <vector>
+
+// using namespace std;
+
+// void input(int& x, vector<int>& vec)
+// {
+// 	cin >> x;
+
+// 	vec.resize(0);
+
+// 	int n;
+// 	while (cin >> n)
+// 	{
+// 		vec.push_back(n);
+// 	}
+// }
+
+// int sequential_search(int x, vector<int>vec)
+// {
+// 	int res = -1, max = -10e6;
+// 	for (int i = 0; i < vec.size(); i++)
+// 		if (vec[i] <= x && max < vec[i]) { res = i, max = vec[i]; };
+
+// 	return res;
+// }
+
+// int main()
+// {
+// 	ios::sync_with_stdio;
+// 	cin.tie(0); cout.tie(0);
+
+// 	int x;
+// 	vector<int> vec;
+
+// 	input(x, vec);
+// 	cout << sequential_search(x, vec);
+
+// 	return 0;
+// }
 
 
 //4
