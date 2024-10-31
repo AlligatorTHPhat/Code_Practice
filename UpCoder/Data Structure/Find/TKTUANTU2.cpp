93bdf29a7203229e2e2438fa172ea979e4e59a86
AlligatorTHPhat <164316763+AlligatorTHPhat@users.noreply.github.com>
2024-10-31 21:48:58 +0700

    Update TKTUANTU2.cpp

--- UpCoder/Data Structure/Find/TKTUANTU2.cpp
+++ UpCoder/Data Structure/Find/TKTUANTU2.cpp
@@ -3,53 +3,105 @@
 
 using namespace std;
 
-vector <int> res;
-
-void input(int& n, int& x, vector<int>& vec)
+template <typename DataType>
+void input(int& _size, DataType& find, DataType values[])
 {
-	cin >> n >> x;
-
-	vec.resize(n);
-
-	for (int i = 0; i < vec.size(); i++)
-		cin >> vec[i];
-
+	cin >> _size >> find;
+	for (int i = 0; i < _size; i++)
+	{
+		cin >> values[i];
+	}
 }
 
-int sequential_search(int n, int x, vector<int>vec)
+template <typename DataType>
+vector<DataType> sequential_find(int _size, DataType find, DataType values[])
 {
-	int result = -1;
-	for (int i = 0; i < vec.size(); i++)
+	vector<DataType> result;
+	for (int i = 0; i < _size; i++)
 	{
-		if (vec[i] == x) { result = i; res.push_back(i); }
+		if (values[i] == find)
+		{
+			result.push_back(i);
+		}
 	}
-
 	return result;
 }
 
-void print_result(int n, int x, vector<int>vec)
+template <typename DataType>
+void print_sequential_result(int _size, DataType find, DataType values[])
 {
-	sequential_search(n, x, vec);
-	if (!res.empty())
-	{
-		for (auto& r : res)
-			cout << r << " ";
-	}
-	else
+	vector<DataType> result = sequential_find(_size, find, values);
+	if (result.empty())
 		cout << -1;
+	else
+	{
+		for (DataType res : result)
+			cout << res << " ";
+	}
 }
 
 int main()
 {
-	int n, x;
-	vector<int> vec;
+	int n, x, values[100];
+	input<int>(n, x, values);
 
-	input(n, x, vec);
-	
-	print_result(n, x, vec);
+	print_sequential_result<int>(n, x, values);
 
 	return 0;
 }
+
+// #include <iostream>
+// #include <vector>
+
+// using namespace std;
+
+// vector <int> res;
+
+// void input(int& n, int& x, vector<int>& vec)
+// {
+// 	cin >> n >> x;
+
+// 	vec.resize(n);
+
+// 	for (int i = 0; i < vec.size(); i++)
+// 		cin >> vec[i];
+
+// }
+
+// int sequential_search(int n, int x, vector<int>vec)
+// {
+// 	int result = -1;
+// 	for (int i = 0; i < vec.size(); i++)
+// 	{
+// 		if (vec[i] == x) { result = i; res.push_back(i); }
+// 	}
+
+// 	return result;
+// }
+
+// void print_result(int n, int x, vector<int>vec)
+// {
+// 	sequential_search(n, x, vec);
+// 	if (!res.empty())
+// 	{
+// 		for (auto& r : res)
+// 			cout << r << " ";
+// 	}
+// 	else
+// 		cout << -1;
+// }
+
+// int main()
+// {
+// 	int n, x;
+// 	vector<int> vec;
+
+// 	input(n, x, vec);
+	
+// 	print_result(n, x, vec);
+
+// 	return 0;
+// }
 
 // 5 3
 // 2 1 3 5 3
