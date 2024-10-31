80a65c2d5434e0031952c667dbd53a81e0dac65c
AlligatorTHPhat <164316763+AlligatorTHPhat@users.noreply.github.com>
2024-10-31 21:31:13 +0700

    Update TKTUANTU.cpp

--- UpCoder/Data Structure/Find/TKTUANTU.cpp
+++ UpCoder/Data Structure/Find/TKTUANTU.cpp
@@ -1,40 +1,76 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
-void input(int& n, int& x, vector<int>& vec)
+template <typename DataType>
+void input(DataType& _size, DataType& find, DataType values[])
 {
-	cin >> n >> x;
-
-	for (int i = 0; i < vec.size(); i++)
-		cin >> vec[i];
-
+	cin >> _size >> find;
+	for (int i = 0; i < _size; i++)
+	{
+		cin >> values[i];
+	}
 }
 
-int sequential_search(int n, int x, vector<int>vec)
+template <typename DataType>
+DataType sequential_find(DataType _size, DataType find, DataType values[])
 {
-	int result = -1;
-	for (int i = 0; i < vec.size(); i++)
+	DataType result = -1;
+	for (int i = 0; i < _size; i++)
 	{
-		if (vec[i] == x) { result = i; break; }
+		if (values[i] == find)
+			result = i;
 	}
-
 	return result;
 }
 
 int main()
 {
-	ios::sync_with_stdio;
-	cin.tie(0); cout.tie(0);
+	int n, x, values[100];
+	input<int>(n, x, values);
 
-	int n, x;
-	vector<int> vec(n);
-	input(n, x, vec);
-	cout << sequential_search(n, x, vec);
+	cout << sequential_find<int>(n, x, values);
 
 	return 0;
 }
+
+// #include <iostream>
+// #include <vector>
+
+// using namespace std;
+
+// void input(int& n, int& x, vector<int>& vec)
+// {
+// 	cin >> n >> x;
+
+// 	for (int i = 0; i < vec.size(); i++)
+// 		cin >> vec[i];
+
+// }
+
+// int sequential_search(int n, int x, vector<int>vec)
+// {
+// 	int result = -1;
+// 	for (int i = 0; i < vec.size(); i++)
+// 	{
+// 		if (vec[i] == x) { result = i; break; }
+// 	}
+
+// 	return result;
+// }
+
+// int main()
+// {
+// 	ios::sync_with_stdio;
+// 	cin.tie(0); cout.tie(0);
+
+// 	int n, x;
+// 	vector<int> vec(n);
+// 	input(n, x, vec);
+// 	cout << sequential_search(n, x, vec);
+
+// 	return 0;
+// }
 
 // 5 3
 // 2 1 3 5 3
