445042aa3024700e604402e8d18f236d6f5e2616
AlligatorTHPhat <164316763+AlligatorTHPhat@users.noreply.github.com>
2024-11-01 22:20:08 +0700

    Update C1_HK18191_101602.cpp

--- UpCoder/Data Structure/Find/C1_HK18191_101602.cpp
+++ UpCoder/Data Structure/Find/C1_HK18191_101602.cpp
@@ -44,3 +44,67 @@ input :
 	5 7 
 	1 9 3 8 2
 */
+
+// #include<bits/stdc++.h>
+
+// using namespace std;
+
+// int sequential_search1(int n, int x, int values[])
+// {
+// 	for (int i = n - 1; i >= 0; i--)
+// 	{
+// 		if (x == values[i])
+// 			return i;
+// 	}
+// 	return -1;
+// }
+
+// int sequential_search2(int n, int x, int values[])
+// {
+// 	int max = INT_MIN;
+// 	for (int i = 0; i < n; i++)
+// 	{
+// 		if (values[i] > x)
+// 			{
+// 			    max = i;
+// 		        return max;
+// 			}
+// 	}
+//     return INT_MIN;
+// }
+
+// int main()
+// {
+// 	int n, x, values[100];
+// 	cin >> n >> x;
+
+// 	for (int i = 0; i < n; i++)
+// 	{
+// 		cin >> values[i];
+// 	}
+
+// 	int Y = sequential_search1(n, x, values);
+// 	if (Y != -1)
+// 	{
+// 		cout << "Y" << endl << Y;
+// 	}
+// 	else
+// 	{
+// 		int X = sequential_search2(n, x, values);
+// 		if (X != INT_MIN)
+// 		{
+// 			cout << "N" << endl << X;
+// 		}
+// 		else
+// 		{
+// 			cout << "N" << endl << -1;
+// 		}
+// 	}
+
+// }
+
+// // 5 7 
+// // 1 6 3 0 2
+
+// // 5 7
+// // 1 9 3 8 2
