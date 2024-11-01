58fcd400ddd51a528aff45b67f91c0cc81f13ff7
AlligatorTHPhat <164316763+AlligatorTHPhat@users.noreply.github.com>
2024-11-01 13:58:31 +0700

    Update TKTuanTu5.cpp

--- UpCoder/Data Structure/Find/TKTuanTu5.cpp
+++ UpCoder/Data Structure/Find/TKTuanTu5.cpp
@@ -1,46 +1,100 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
-int n, x, y;
-vector<int> vec;
-
-void input(int& n, int& x, int& y, vector<int>& vec) {
-    cin >> n >> x >> y;
-    vec.resize(n);
-    for (int i = 0; i < vec.size(); i++)
-        cin >> vec[i];
+void input(int& _size, int& find, int& dev, int values[])
+{
+	cin >> _size >> find >> dev;
+	for (int i = 0; i < _size; i++)
+	{
+		cin >> values[i];
+	}
 }
 
-int relative_search(int n, int x, int y, vector<int> vec)
+int sequential_search(int _size, int find, int dev, int values[])
 {
-    int closet_index = -1;
-    int min_difference = y + 1;
+	int result = -1;
+	for (int i = 0; i < _size; i++)
+	{
+		if (values[i] == find)
+		{
+			return i;
+		}
+	}
 
-    for (int i = 0; i < vec.size(); i++)
-    {
-        if (vec[i] == x) return i;
-        else if (abs(x - vec[i]) <= y)
-        {
-            int difference = abs(x - vec[i]);
-            if (min_difference > difference)
-            {
-                closet_index = i;
-                min_difference = difference;
-            }
-        }
-    }
-    return closet_index;
+	int a = find, b = find;
+	while (a >= find - dev && b <= find + dev)
+	{
+		for (int i = 0; i < _size; i++)
+		{
+			if (values[i] == a || values[i] == b)
+				return i;
+		}
+		a--, b++;
+	}
+	return -1;
+}
+
+void print_sequential_search(int _size, int find, int dev, int values[])
+{
+	cout << sequential_search(_size, find, dev, values);
 }
 
 int main()
 {
-    input(n, x, y, vec);
-    cout << relative_search(n, x, y, vec);
+	int n;
+	int x, y, values[100];
 
-    return 0;
+	input(n, x, y, values);
+
+	print_sequential_search(n, x, y, values);
+
+	return 0;
 }
+
+// #include <iostream>
+// #include <vector>
+
+// using namespace std;
+
+// int n, x, y;
+// vector<int> vec;
+
+// void input(int& n, int& x, int& y, vector<int>& vec) {
+//     cin >> n >> x >> y;
+//     vec.resize(n);
+//     for (int i = 0; i < vec.size(); i++)
+//         cin >> vec[i];
+// }
+
+// int relative_search(int n, int x, int y, vector<int> vec)
+// {
+//     int closet_index = -1;
+//     int min_difference = y + 1;
+
+//     for (int i = 0; i < vec.size(); i++)
+//     {
+//         if (vec[i] == x) return i;
+//         else if (abs(x - vec[i]) <= y)
+//         {
+//             int difference = abs(x - vec[i]);
+//             if (min_difference > difference)
+//             {
+//                 closet_index = i;
+//                 min_difference = difference;
+//             }
+//         }
+//     }
+//     return closet_index;
+// }
+
+// int main()
+// {
+//     input(n, x, y, vec);
+//     cout << relative_search(n, x, y, vec);
+
+//     return 0;
+// }
 
 // 7 5 2
 // 10 2 8 7 4 6 1
