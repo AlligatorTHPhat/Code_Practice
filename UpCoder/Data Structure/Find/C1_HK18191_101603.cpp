5e4f623d7b0bd1ca277cb1805bd36ba3d13be0ba
AlligatorTHPhat <164316763+AlligatorTHPhat@users.noreply.github.com>
2024-11-01 22:19:26 +0700

    Update C1_HK18191_101603.cpp

--- UpCoder/Data Structure/Find/C1_HK18191_101603.cpp
+++ UpCoder/Data Structure/Find/C1_HK18191_101603.cpp
@@ -1,51 +1,121 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 
 using namespace std;
 
-void input(int& n, int& x, int values[]) {
-	cin >> n >> x;
+int sequential_search1(int n, int x, int values[])
+{
 	for (int i = 0; i < n; i++)
+	{
+		if (x == values[i])
+			return i;
+	}
+
+	return -1;
+}
+
+int sequential_search2(int n, int x, int values[])
+{
+	int max = INT_MIN;
+	for (int i = n - 1; i >= 0; i--)
+	{
+		if (max < values[i] && values[i] < x)
+			max = values[i];
+	}
+
+	for (int i = n - 1; i >= 0; i--)
+	{
+		if (max == values[i])
+			return i;
+	}
+
+	return max;
+}
+
+int main()
+{
+	int n, x, values[100];
+	cin >> n >> x;
+
+	for (int i = 0; i < n; i++)
+	{
 		cin >> values[i];
-	return;
-}
+	}
 
-void linear_search(int n ,int x, int values[]) {
-	for (int i = 0; i < n; i++) {
-		if (values[i] == x) {
-			cout << "Y" << endl << i;
-			return;
+	int Y = sequential_search1(n, x, values);
+	if (Y != -1)
+	{
+		cout << "Y" << endl << Y;
+	}
+	else
+	{
+		int X = sequential_search2(n, x, values);
+		if (X != INT_MIN)
+		{
+			cout << "N" << endl << X;
+		}
+		else
+		{
+			cout << "N" << endl << -1;
 		}
 	}
+
+}
+
+// 8 52
+// 41 59 26 53 58 97 93 41
+
+// 5 7
+// 10 9 13 8 12
+
+
+// #include <bits/stdc++.h>
+
+// using namespace std;
+
+// void input(int& n, int& x, int values[]) {
+// 	cin >> n >> x;
+// 	for (int i = 0; i < n; i++)
+// 		cin >> values[i];
+// 	return;
+// }
+
+// void linear_search(int n ,int x, int values[]) {
+// 	for (int i = 0; i < n; i++) {
+// 		if (values[i] == x) {
+// 			cout << "Y" << endl << i;
+// 			return;
+// 		}
+// 	}
 	
-	int k = INT_MIN;
-	for (int i = n - 1; i >= 0; i--) {
-		if (values[i] < x && values[i] > k) {
-			k = values[i];
-		}
-	}
+// 	int k = INT_MIN;
+// 	for (int i = n - 1; i >= 0; i--) {
+// 		if (values[i] < x && values[i] > k) {
+// 			k = values[i];
+// 		}
+// 	}
 
-	if (k != INT_MIN) {
-		for (int i = n - 1; i >= 0; i--) {
-			if (values[i] == k) {
-				cout << "N" << endl << i;
-				return;
-			}
-		}
-	}
-	else cout << "N" << endl << -1; return;
-}
+// 	if (k != INT_MIN) {
+// 		for (int i = n - 1; i >= 0; i--) {
+// 			if (values[i] == k) {
+// 				cout << "N" << endl << i;
+// 				return;
+// 			}
+// 		}
+// 	}
+// 	else cout << "N" << endl << -1; return;
+// }
 
-int main() {
-	int n, x;
-	int values[100];
+// int main() {
+// 	int n, x;
+// 	int values[100];
 
-	input(n, x, values);
+// 	input(n, x, values);
 
-	linear_search(n, x, values);
+// 	linear_search(n, x, values);
 
-	return 0;
+// 	return 0;
 
-}
+// }
 /*
 input :
 	8 52
