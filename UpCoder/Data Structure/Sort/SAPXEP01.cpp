e30fc9e9fa14df1d94cebeb9642668b4ad05f3ef
AlligatorTHPhat <164316763+AlligatorTHPhat@users.noreply.github.com>
2024-11-03 02:32:20 +0700

    Update SAPXEP01.cpp

--- UpCoder/Data Structure/Sort/SAPXEP01.cpp
+++ UpCoder/Data Structure/Sort/SAPXEP01.cpp
@@ -1,5 +1,3 @@
-//do truyện của anh làm cũng khá dài, nên anh nghĩ là nên làm 1 cái oneshot thử lửa trước rồi mới up truyện gốc
-
 #include <iostream>
 
 using namespace std;
@@ -8,119 +6,163 @@ template <typename DataType>
 void input(int& n, DataType values[])
 {
 	cin >> n;
+
 	for (int i = 0; i < n; i++)
 		cin >> values[i];
 }
 
 template <typename DataType>
-void interchange_sort(int n, DataType values[])
+void selection_sort(int n, DataType values[])
 {
-	int mid = n / 2;
-	for (int i = 0; i < mid; i++)
+	for (int i = 0; i < n; i++)
 	{
-		for (int j = i + 1; j < mid; j++)
+		int min = i;
+		for (int j = i + 1; j < n; j++)
 		{
-			if (values[j] < values[i])
-				swap(values[j], values[i]);
+			if (values[min] > values[j])
+				swap(values[min], values[j]);
 		}
 	}
 }
 
 template <typename DataType>
-void reverse_sort(int n, DataType values[])
+void reverse_selection_sort(int n, int mid, DataType values[])
 {
-	int mid = (n % 2 == 0) ? n / 2 : n / 2 + 1;
+	for (int i = mid; i < n; i++)
+	{
+		int min = i;
+		for (int j = i + 1; j < n; j++)
+		{
+			if (values[min] < values[j])
+				swap(values[min], values[j]);
+		}
+	}
+}
+
+template <typename DataType>
+void interchange_sort(int n, DataType values[])
+{
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i + 1; j < n; j++)
+		{
+			if (values[i] > values[j])
+				swap(values[i], values[j]);
+		}
+	}
+}
+
+template <typename DataType>
+void reverse_interchange_sort(int n, int mid, DataType values[])
+{
 	for (int i = mid; i < n; i++)
 	{
 		for (int j = i + 1; j < n; j++)
 		{
-			if (values[j] > values[i])
-				swap(values[j], values[i]);
+			if (values[i] < values[j])
+				swap(values[i], values[j]);
 		}
 	}
 }
 
 template <typename DataType>
-void print_result(int n, DataType values[])
+void bubble_sort(int n, DataType values[])
 {
-	interchange_sort(n, values);
-	reverse_sort(n, values);
-
 	for (int i = 0; i < n; i++)
 	{
-		cout << values[i] << " ";
+		bool swapped = false;
+		for (int j = 0; j < n - i - 1; j++)
+			if (values[j] > values[j + 1])
+			{
+				swap(values[j], values[j + 1]);
+				swapped = true;
+			}
+		if (swapped == false)
+			break;
 	}
-	cout << endl;
+}
+
+template <typename DataType>
+void reverse_bubble_sort(int n, int mid, DataType values[])
+{
+	for (int i = mid; i < n; i++)
+	{
+		bool swapped = false;
+		for (int j = mid; j < n - (i - mid) - 1; j++)
+		
+			if (values[j] < values[j + 1])
+			{
+				swap(values[j], values[j + 1]);
+				swapped = true;
+			}
+		if (swapped == false)
+			break;
+	}
+}
+
+template <typename DataType>
+void insertion_sort(int n, DataType values[])
+{
+	for (int i = 0; i < n; i++)
+	{
+		DataType key = values[i];
+
+		int j = i - 1;
+		while (j >= 0 && values[j] > key)
+		{
+			values[j + 1] = values[j];
+			j--;
+		}
+		values[j + 1] = key;
+	}
+}
+
+template <typename DataType>
+void reverse_insertion_sort(int n, int mid, DataType values[]) 
+{
+	for (int i = mid; i < n; i++)
+	{
+		DataType key = values[i];
+		int j = i - 1;
+		while (j >= mid && values[j] < key)
+		{
+			values[j + 1] = values[j];
+			j--;
+		}
+		values[j + 1] = key;
+	}
+}
+
+template <typename DataType>
+void output(int n, DataType values[])
+{
+	int mid = (n % 2 == 0) ? n / 2 : (n / 2) + 1;
+
+	//selection_sort(mid, values);
+	//reverse_selection_sort(n, mid, values);
+
+	//interchange_sort(mid, values);
+	//reverse_interchange_sort(n, mid, values);
+
+	bubble_sort(mid, values);
+	reverse_bubble_sort(n, mid, values);
+
+	//insertion_sort(mid, values);
+	//reverse_insertion_sort(n, mid, values);
+
+	for (int i = 0; i < n; i++)
+		cout << values[i] << " ";
 }
 
 int main()
 {
 	int n;
+
 	int values[100];
 
 	input(n, values);
-	print_result(n, values);
+
+	output(n, values);
 
 	return 0;
 }
-
-
-
-// #include <iostream>
-
-// using namespace std;
-
-// int array_size, values[100];
-
-// /*
-// void swap(dataptype& a, datatype& b) {
-// 		datatype temp = a;
-// 		a = b;
-// 		b = temp;
-// 		return;
-// }
-// */
-
-// void input(int& array_size, int values[]) {
-// 	cin >> array_size;
-// 	for (int i = 0; i < array_size; i++) {
-// 		cin >> values[i];
-// 	} return;
-// }
-
-// void Insertion_Sort(int array_size, int values[]) {
-// 	int j;
-// 	for (int i = 1; i < array_size / 2; i++) {
-// 		j = i;
-// 		while ((j > 0) && (values[j - 1] > values[j])) {
-// 			swap(values[j - 1], values[j]);
-// 			j--;
-// 		} 
-// 	} return;
-// }
-
-// void Reverse_Sort(int array_size, int values[]) {
-// 	int start = (array_size + 1) / 2, j;
-// 	for (int i = start + 1; i < array_size; i++) {
-// 		j = i;
-// 		while ((j > start) && (values[j - 1] < values[j])) {
-// 			swap(values[j - 1], values[j]);
-// 			j--;
-// 		}
-// 	} return;
-// }
-
-// void output(int array_size, int values[]) {
-// 	for (int i = 0; i < array_size; i++)
-// 		cout << values[i] << " ";
-// 	return;
-// }
-
-// int main() {
-// 	input(array_size, values);
-// 	Insertion_Sort(array_size, values);
-// 	Reverse_Sort(array_size, values);
-// 	output(array_size, values);
-	
-// 	return 0;
-// }
