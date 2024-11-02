8fb9fe20945283f4fac85927c4ecc4a4c55ab236
AlligatorTHPhat <164316763+AlligatorTHPhat@users.noreply.github.com>
2024-11-03 01:25:10 +0700

    Update SXCHON.cpp

--- UpCoder/Data Structure/Sort/SXCHON.cpp
+++ UpCoder/Data Structure/Sort/SXCHON.cpp
@@ -1,158 +1,146 @@
 #include <iostream>
-using namespace std;
 
-template <typename DataType>
+using namespace std;
 
 /*
 void swap(dataptype& a, datatype& b) {
-        datatype temp = a;
-        a = b;
-        b = temp;
-        return;
+		datatype temp = a;
+		a = b;
+		b = temp;
+		return;
 }
 */
 
+template <typename DataType>
 void input(int& n, DataType values[])
 {
-    n = 0;
-    int temp;
-    while (cin >> temp)
-    {
-        values[n++] = temp;
-    }
+	n = 0;
+
+	DataType temp;
+	while (cin >> temp)
+		values[n++] = temp;
 }
 
 template <typename DataType>
 void selection_sort(int n, DataType values[])
 {
-    int min;
-    for (int i = 0; i < n - 1; i++)
-    {
-        min = i;
-        for (int j = i + 1; j < n; j++)
-        {
-            if (values[j] < values[min])
-            {
-                min = j;
-            }
-        }
-        swap(values[i], values[min]);
-    }
+	for (int i = 0; i < n; i++)
+	{
+		int min = i;
+		for (int j = i + 1; j < n; j++)
+			if (values[min] > values[j])
+				swap(values[min], values[j]);
+	}
 }
 
 template <typename DataType>
 void interchange_sort(int n, DataType values[])
 {
-    for (int i = 0; i < n - 1; i++)
-    {
-        for (int j = i + 1; j < n; j++)
-        {
-            if (values[j] < values[i])
-            {
-                swap(values[j], values[i]);
-            }
-        }
-    }
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i + 1; j < n; j++)
+			if (values[i] > values[j])
+				swap(values[i], values[j]);
+	}
 }
 
 template <typename DataType>
-void bubble_sort(int n, DataType values[])
+void bubble_sort(int n, DataType values)
 {
-    for (int i = 0; i < n - 1; i++)
-    {
-        bool swapped = false;
-        for (int j = 0; j < n - i - 1; j++)
-        {
-            if (values[j + 1] < values[j])
-            {
-                swap(values[j + 1], values[j]);
-                swapped = true;
-            }
-        }
-        if (!swapped)
-            break;
-    }
+	for (int i = 0; i < n; i++)
+	{
+		bool swapped = false;
+		for (int j = 0; j < n - i - 1; j++)
+			if (values[j] > values[j + 1])
+			{
+				swap(values[j], values[j + 1]);
+				bool swapped = false;
+			}
+		if (swapped == true)
+			break;
+	}
+}
+
+template <typename DataType>
+void insertion_sort(int n, DataType values[])
+{
+	for (int i = 0; i < n; i++)
+	{
+		DataType key = values[i];
+		
+		int j = i - 1;
+		while (j >= 0 && values[j] > key)
+		{
+			values[j + 1] = values[j];
+			j--;
+		}
+		values[j + 1] = key;
+	}
 }
 
 template <typename DataType>
 int partition(int start, int end, DataType values[])
 {
-    DataType pivot = values[start];
+	DataType pivot = values[start];
 
-    int count = 0;
+	int count = 0;
 
-    for (int i = start + 1; i <= end; i++)
-    {
-        if (values[i] <= pivot)
-            count++;
-    }
+	for (int i = start + 1; i <= end; i++)
+	{
+		if (values[i] <= pivot)
+			count++;
+	}
 
-    int pivot_index = start + count;
+	int pivot_index = start + count;
 
-    swap(values[pivot_index], values[start]);
+	swap(values[pivot_index], values[start]);
 
-    int i = start, j = end;
+	int i = start, j = end;
 
-    while (i<pivot_index && j >pivot_index)
-    {
-        while (values[i] < pivot)
-            i++;
-        while (values[j] > pivot)
-            j--;
-        if (i < pivot_index && j > pivot_index)
-            swap(values[i++], values[j--]);
-    }
+	while (i<pivot_index && j >pivot_index)
+	{
+		while (values[i] < pivot)
+			i++;
+		while (values[j] > pivot)
+			j--;
+		if (i < pivot_index && j > pivot_index)
+			swap(values[i++], values[j--]);
+	}
 
-    return pivot_index;
-}
-
-template <typename DataType>
-void insertion_Sort(int n, DataType values[])
-{
-    for (int i = 1; i < n; i++)
-    {
-        DataType key = values[i];
-        int j = i - 1;
-
-        while (j >= 0 && values[j] > key) {
-            values[j + 1] = values[j];
-            j = j - 1;
-        }
-        values[j + 1] = key;
-    }
+	return pivot_index;
 }
 
 template <typename DataType>
 void quick_sort(int left, int right, DataType values[])
 {
-    if (left < right)
-    {
-        int pivot = partition(left, right, values);
-        quick_sort(left, right - 1, values);
-        quick_sort(left + 1, right, values);
-    }
+	if (left < right)
+	{
+		int pivot = partition(left, right, values);
+		quick_sort(left, right - 1, values);
+		quick_sort(left + 1, right, values);
+	}
 }
 
 template <typename DataType>
-void print_result(int n, DataType values[])
+void output(int n, DataType values)
 {
-    // selection_sort(n, values);
-    // interchange_sort(n, values);
-    //bubble_sort(n, values);
-    insertion_Sort(n, values); 
-    //quick_sort(0, n - 1, values);
+	//selection_sort(n, values);
+	//interchange_sort(n, values);
+	//bubble_sort(n, values);
+	insertion_sort(n, values);
+	//quick_sort(n, values);
 
-    for (int i = 0; i < n; i++)
-    {
-        cout << values[i] << " ";
-    }
-    cout << endl;
+	for (int i = 0; i < n; i++)
+		cout << values[i] << " ";
 }
 
 int main()
 {
-    int n, values[100];
-    input(n, values);
-    print_result(n, values);
-    return 0;
+	int n;
+
+	int values[100];
+
+	input(n, values);
+
+	output(n, values);
 }
